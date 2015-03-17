5f53dbdc0b06734bbcf62f2313baa66dda95ff2f
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-17 23:06:36 +0800

    modify file pash

--- specs/reduce/find_element_first_subscript_spec.js
+++ specs/reduce/find_element_first_subscript_spec.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var find_element_subscript = require('../../practices/reduce/find_element_subscript.js');
+var find_element_first_subscript = require('../../practices/reduce/find_element_first_subscript.js');
 
-describe('find_element_subscript', function() {
+describe('find_element_first_subscript', function() {
 
   var collection = [1,11,27,20,4,9,15,4,1,11];
   var element = 4;
 
   it('找出某元素在给定集合中的第一个下标', function() {
-    var result = find_element_subscript(collection, element);
+    var result = find_element_first_subscript(collection, element);
 
     expect(result).toEqual(4);
   });
