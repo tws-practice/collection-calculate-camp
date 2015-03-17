033ad6a93e40de85d5c0c70f38fe696b7f349535
fgcui <fgcui1204@163.com>
2015-03-17 19:47:14 +0800

    modify collection data

--- specs/map/map_to_three_multiples_spec.js
+++ specs/map/map_to_three_multiples_spec.js
@@ -2,8 +2,8 @@
 var map_to_three_multiples = require('../../practices/map/map_to_three_multiples.js');
 
 describe('map to three multiples',function(){
-  var collection_a = [1,2,3,4];
-  var collection_b = [3,6,9,12];
+  var collection_a = [1,3,5,4,9];
+  var collection_b = [3,9,15,12,27];
 
   it('三倍映射',function(){
     var result = map_to_three_multiples(collection_a);
