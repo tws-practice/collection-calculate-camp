1e94cc146a4ec8b77ba05c67cc614694fa420fe7
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-17 17:38:19 +0800

    add module collect max number

---
+++ specs/reduce/collect_max_number_spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var collect_max_number = require('../../practices/reduce/collect_max_number.js');
+
+describe('collect_max_number', function () {
+
+
+  var collection_a = [1, 2, 3, 4, 5];
+
+  it('选出给定数字集合元素的最大值', function() {
+    var result = collect_max_number(collection_a);
+
+    expect(result).toEqual(5);
+  });
+});
