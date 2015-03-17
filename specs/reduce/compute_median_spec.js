5a89a289006ba68a40ed2e8b4f7316d4cf69d64e
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-17 21:54:25 +0800

    complete compute_median_spec test code

--- specs/reduce/compute_median_spec.js
+++ specs/reduce/compute_median_spec.js
@@ -5,6 +5,7 @@ var compute_median = require('../../practices/reduce/compute_median.js');
 describe('compute_median', function () {
   var collection_a = [1, 1, 1, 2, 3];
   var collection_b = [1, 1, 2, 3];
+  var collection_c = [1,4,6,2,3,10,9,8,11,20,19,30];
 
   it('计算给定奇数数字集合的中位数', function () {
 
@@ -16,5 +17,11 @@ describe('compute_median', function () {
 
     var result = compute_median(collection_b);
     expect(result).toEqual(1.5);
+  });
+
+  it('计算给定偶数数字集合的中位数', function () {
+
+    var result = compute_median(collection_c);
+    expect(result).toEqual(8.5);
   })
 });
