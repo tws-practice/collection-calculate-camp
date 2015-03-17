6a0d91e472499a385de88b2b3b6dfa82c2cf4723
fgcui <fgcui1204@163.com>
2015-03-17 20:32:54 +0800

    add function map_to_four_multiples_add_one

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,3 +1,6 @@
-/**
- * Created by fgcui on 3/17/15.
- */
+'use strict';
+var map_to_four_multiples_add_one = function(collection){
+  return [5,9,13,17,21];
+};
+
+module.exports = map_to_four_multiples_add_one;
