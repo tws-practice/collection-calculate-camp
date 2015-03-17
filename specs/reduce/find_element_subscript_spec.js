21e37f61c4abd636e23a33b6d8fd899f41811dda
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-17 22:17:59 +0800

    complete find element subscript test code

--- specs/reduce/find_element_subscript_spec.js
+++ specs/reduce/find_element_subscript_spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by zhzhang on 3/17/15.
- */
+'use strict';
+
+var find_element_subscript = require('../../practices/reduce/find_element_subscript.js');
+
+describe('find_element_subscript', function() {
+
+  var collection = [1,11,27,20,4,9,15,4,1,11];
+  var element = 4;
+
+  it('找出某元素在给定集合中的第一个下标', function() {
+    var result = find_element_subscript(collection, element);
+
+    expect(result).toEqual(4);
+  });
+});
