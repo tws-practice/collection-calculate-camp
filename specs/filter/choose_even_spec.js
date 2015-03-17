afe319985ba5197fd7682b1c0eedcbcefe76499c
yfwang <yufei930101@gmail.com>
2015-03-17 17:09:31 +0800

    add choose_even test

--- specs/filter/choose_even_spec.js
+++ specs/filter/choose_even_spec.js
@@ -1,3 +1,14 @@
-/**
- * Created by yfwang on 3/17/15.
- */
+'use strict';
+
+var choose_even = require("../../practices/filter/choose_even.js");
+
+describe('choose_even', function() {
+
+  var collection = [0, 1, 2, 3, 4, 6];
+
+  it('应该从collection中选出偶数', function() {
+    var result = choose_even(collection);
+
+    expect(result).toEqual([0, 2, 4, 6]);
+  })
+});
