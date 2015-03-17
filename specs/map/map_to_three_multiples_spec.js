7b859eab39f7c64797400886a203771c4e1f8a68
fgcui <fgcui1204@163.com>
2015-03-17 19:41:34 +0800

    add test map to three multiples

--- specs/map/map_to_three_multiples_spec.js
+++ specs/map/map_to_three_multiples_spec.js
@@ -1,3 +1,12 @@
-/**
- * Created by fgcui on 3/17/15.
- */
+'use strict';
+var map_to_three_multiples = require('../../practices/map/map_to_three_multiples.js');
+
+describe('map to three multiples',function(){
+  var collection_a = [1,2,3,4];
+  var collection_b = [3,6,9,12];
+
+  it('三倍映射',function(){
+    var result = map_to_three_multiples(collection_a);
+    expect(result).toEqual(collection_b);
+  });
+});
