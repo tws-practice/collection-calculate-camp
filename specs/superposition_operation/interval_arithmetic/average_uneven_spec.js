e46e02ca00cf40b75c387bbf73655ca13ba9d23a
yfwang <yufei930101@gmail.com>
2015-03-18 11:21:48 +0800

    add average_uneven test

--- specs/superposition_operation/interval_arithmetic/average_uneven_spec.js
+++ specs/superposition_operation/interval_arithmetic/average_uneven_spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by yfwang on 3/18/15.
- */
+'use strict';
+
+var average_uneven = require("../../practices/superposition_operation/interval_arithmetic/average_uneven.js");
+
+describe('average_uneven', function() {
+
+  var collection = [1,2,3,4,5,6,7,8,9,10];
+
+  it('1-10的奇数算平均数', function() {
+
+    var result = average_uneven(collection);
+
+    expect(result).toEqual(5);
+  })
+});
