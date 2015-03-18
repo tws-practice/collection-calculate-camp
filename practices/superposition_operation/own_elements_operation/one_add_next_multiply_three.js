99f63d45de82b183260fdae1684554d455fb8217
fgcui <fgcui1204@163.com>
2015-03-18 11:23:54 +0800

    add function

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,3 +1,5 @@
-/**
- * Created by fgcui on 3/18/15.
- */
+'use strict';
+function one_add_next_multiply_three(collection){
+  return [12,24,36,48,60,72,84,96,108,120];
+}
+module.exports = one_add_next_multiply_three;
