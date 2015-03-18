1a0e8490a6846ecd7214b8229d294514cd7c6d31
yfwang <yufei930101@gmail.com>
2015-03-18 09:42:20 +0800

    new double_dimensional_array_to_one_spec.js file

--- specs/filter/two_collections_practice_3_spec.js
+++ specs/filter/two_collections_practice_3_spec.js
@@ -1,3 +1,16 @@
-/**
- * Created by yfwang on 3/18/15.
- */
+'use strict';
+
+var choose_divisible_integer = require();
+
+describe('choose_divisible_integer', function() {
+
+  var collection_a = [4,9,25,16,21,64,32,35];
+  var collection_b = [2,3,5];
+
+  it('选出A集合中可以被B集合中整除的数', function() {
+    var result = choose_divisible_integer(collection_a, collection_b);
+
+    expect(result).toEqual([])
+  })
+
+})
