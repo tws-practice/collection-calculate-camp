190aa88e2cc6cfec1b7abad078f6b8b01e027c50
yfwang <yufei930101@gmail.com>
2015-03-18 10:09:02 +0800

    add double_to_one test

--- specs/flaten/double_dimensional_array_to_one_spec.js
+++ specs/flaten/double_dimensional_array_to_one_spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by yfwang on 3/18/15.
- */
+'use strict';
+
+var double_to_one = require("../../practices/filter/double_dimensional_array_to_one.js");
+
+descirbe('double_to_one', function() {
+
+  var collection = [1, [2], [3, 4]];
+
+  it('把二维数组变成一维数组', function() {
+
+    var result = double_to_one(collection);
+
+    expect(result).toEqual([1, 2, 3, 4]);
+  })
+});
