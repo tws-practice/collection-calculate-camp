be023cff41124bf9bdf0efe1dba40a3e474033c0
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-18 11:24:46 +0800

    modify description of test code

--- specs/collectionOperator/get_integer_interval_spec.js
+++ specs/collectionOperator/get_integer_interval_spec.js
@@ -1,21 +1,27 @@
 'use strict';
 
-var get_integer_interval = require('../../practices/reduce/get_integer_interval.js');
+var get_integer_interval = require('../../practices/collectionOperator/get_integer_interval.js');
 
 describe('get_integer_interval', function () {
 
   var collection_a = [1, 2, 3, 4, 5];
   var collection_b = [5, 4, 3, 2, 1];
 
-  it('', function() {
+  it('根据给出的两个数字得到一个自增的数字区间', function() {
 
     var result = get_integer_interval(1, 5);
     expect(result).toEqual(collection_a);
   });
 
-  it('选出给定数字集合元素的最大值', function() {
+  it('根据给出的两个数字得到一个自减的数字区间', function() {
 
     var result = get_integer_interval(5, 1);
     expect(result).toEqual(collection_b);
+  });
+
+  it('给定两个相同的数字得到只含此数字的数组', function() {
+
+    var result = get_integer_interval(5, 5);
+    expect(result).toEqual([5]);
   });
 });
