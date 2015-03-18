bb920551319f74b8db89a322acdc29558cebb590
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-18 10:28:31 +0800

    perfect test code

--- specs/reduce/compute_median_spec.js
+++ specs/reduce/compute_median_spec.js
@@ -5,7 +5,7 @@ var compute_median = require('../../practices/reduce/compute_median.js');
 describe('compute_median', function () {
   var collection_a = [1, 1, 1, 2, 3];
   var collection_b = [1, 1, 2, 3];
-  var collection_c = [1,4,6,2,3,10,9,8,11,20,19,30];
+  var collection_c = [1, 4, 6, 2, 3, 10, 9, 8, 11, 20, 19, 30];
 
   it('计算给定奇数数字集合的中位数', function () {
 
