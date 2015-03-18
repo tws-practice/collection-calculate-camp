55e4f49e3bf902c8a704e72271c8c0e1fddccf42
fgcui <fgcui1204@163.com>
2015-03-18 11:50:34 +0800

    add route require

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
@@ -1,5 +1,5 @@
 'use strict';
-var calculate_median = require('../../../practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js');
+var calculate_median = require('../../../../practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js');
 
 describe('calculate_median_spec',function(){
   var collection_a = [1,2,3,4,5,6];
