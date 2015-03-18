86571f1caa8ad514210c8fab6a511360c3e3fc0f
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-18 10:26:19 +0800

    perfect test code

--- specs/reduce/compute_chain_median_spec.js
+++ specs/reduce/compute_chain_median_spec.js
@@ -3,6 +3,7 @@
 var compute_chain_median = require('../../practices/reduce/compute_chain_median.js');
 
 describe('compute_chain_median', function () {
+
   var chain = '1->4->6->2->3->10->9->8->11->20->19->30';
 
   it('计算给定链表的中位数', function () {
