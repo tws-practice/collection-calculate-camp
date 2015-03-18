9a92fa562469f315b1029d3412e1c4f08fb40b3f
fgcui <fgcui1204@163.com>
2015-03-18 09:40:46 +0800

    modify [a,b,c,d,e] to ['a',b','c','d','e']

--- specs/map/number_map_to_word_spec.js
+++ specs/map/number_map_to_word_spec.js
@@ -3,7 +3,7 @@ var number_map_to_word = require('../../practices/map/number_map_to_word.js');
 
 describe('number map to word',function(){
   var collection_a = [1,2,3,4,5];
-  var collection_b = [a,b,c,d,e];
+  var collection_b = ['a','b','c','d','e'];
 
   it('数字映射为字母', function () {
     var result = number_map_to_word(collection_a);
