bb6db8c7074bd2f449e4f075fecb535bb49ca1af
yfwang <yufei930101@gmail.com>
2015-03-18 10:17:15 +0800

    add double_to_one test

--- specs/flaten/double_dimensional_array_to_one_2_spec.js
+++ specs/flaten/double_dimensional_array_to_one_2_spec.js
@@ -1,3 +1,15 @@
-/**
- * Created by yfwang on 3/18/15.
- */
+'use strict';
+
+var double_to_one = require("../../practices/filter/double_dimensional_array_to_one_2.js");
+
+describe('double_to_one', function() {
+
+  var collection = [[1, 2, 3], [5, 2, 1, 4], [2, 1]];
+
+  it('消除重复,按照第一次出现的顺序排列最后的输出结果', function() {
+
+    var result = double_to_one(collection);
+
+    expect(result).toEqual([1,2,3,5,4]);
+  })
+});
