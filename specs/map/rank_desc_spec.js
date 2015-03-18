9220b2b962dceab21aedb772934d048926a5f32f
fgcui <fgcui1204@163.com>
2015-03-18 10:35:12 +0800

    test function

--- specs/map/rank_desc_spec.js
+++ specs/map/rank_desc_spec.js
@@ -1,3 +1,12 @@
-/**
- * Created by fgcui on 3/18/15.
- */
+'use strict';
+var rank_desc = require('../../practices/map/rank_desc.js');
+
+describe('rank by desc',function(){
+  var collection_a = [3,2,4,5,6];
+  var collection_b = [6,5,4,3,2];
+
+  it('从大到小排序',function(){
+    var result = rank_desc(collection_a);
+    expect(result).toEqual(collection_b);
+  });
+});
