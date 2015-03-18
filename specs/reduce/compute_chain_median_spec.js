342433c6f7c45f7fe05030c612af21dd89f26cee
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-18 10:09:04 +0800

    modify chain

--- specs/reduce/compute_chain_median_spec.js
+++ specs/reduce/compute_chain_median_spec.js
@@ -1,3 +1,13 @@
-/**
- * Created by zhzhang on 3/18/15.
- */
+'use strict';
+
+var compute_median = require('../../practices/reduce/compute_chain_median.js');
+
+describe('compute_median', function () {
+  var chain = '1->4->6->2->3->10->9->8->11->20->19->30';
+
+  it('计算给定链表的中位数', function () {
+
+    var result = compute_median(chain);
+    expect(result).toEqual(1);
+  });
+});
