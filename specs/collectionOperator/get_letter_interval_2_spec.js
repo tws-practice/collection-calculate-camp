0bf2a0454caf5451ef9ffd9e6c01b79197ee6cd4
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-18 13:26:16 +0800

    create get_letter_interval_2 file

--- specs/collectionOperator/get_letter_interval_2_spec.js
+++ specs/collectionOperator/get_letter_interval_2_spec.js
@@ -1,27 +1,36 @@
 'use strict';
 
-var get_integer_interval = require('../../practices/collectionOperator/get_integer_interval.js');
+var get_letter_interval = require('../../practices/collectionOperator/get_letter_interval_2.js');
 
-describe('get_integer_interval', function () {
+describe('get_letter_interval', function () {
 
-  var collection_a = [1, 2, 3, 4, 5];
-  var collection_b = [5, 4, 3, 2, 1];
+  var collection_a = ['t', 'u', 'v', 'w', 'x',
+    'y', 'z', 'aa', 'ab', 'ac', 'ad', 'ae', 'af', 'ag',
+    'ah', 'ai', 'aj', 'ak', 'al', 'am', 'an', 'ao', 'ap',
+    'aq', 'ar', 'as', 'at', 'au', 'av', 'aw', 'ax', 'ay',
+    'az', 'ba'
+  ];
+  var collection_b = ['ba', 'az', 'ay', 'ax', 'aw', 'av', 'au', 'at',
+    'as', 'ar', 'aq', 'ap', 'ao', 'an', 'am', 'al', 'ak', 'aj', 'ai',
+    'ah', 'ag', 'af', 'ae', 'ad', 'ac', 'ab', 'aa', 'z', 'y', 'x',
+    'w', 'v', 'u', 't'
+  ];
 
-  it('根据给出的两个数字得到一个自增1的数字区间', function() {
+  it('根据给出的两个数字得到对应自增的字母区间', function() {
 
-    var result = get_integer_interval(1, 5);
+    var result = get_letter_interval(20, 53);
     expect(result).toEqual(collection_a);
   });
 
-  it('根据给出的两个数字得到一个自减1的数字区间', function() {
+  it('根据给出的两个数字得到对应自减的字母区间', function() {
 
-    var result = get_integer_interval(5, 1);
+    var result = get_letter_interval(53, 20);
     expect(result).toEqual(collection_b);
   });
 
-  it('给定两个相同的数字得到只含此数字的数组', function() {
+  it('给定两个相同的数字得到只含此数字对应字母的数组', function() {
 
-    var result = get_integer_interval(5, 5);
-    expect(result).toEqual([5]);
+    var result = get_letter_interval(28, 28);
+    expect(result).toEqual(['ab']);
   });
 });
