9c7f6dd3b8771bfabcc79e8943bb4ba3c3d99a15
fgcui <fgcui1204@163.com>
2015-03-18 10:16:29 +0800

    add test

--- specs/map/number_map_to_word_over_26_spec.js
+++ specs/map/number_map_to_word_over_26_spec.js
@@ -4,7 +4,7 @@ var number_map_to_word_over_26 = require('../../practices/map/number_map_to_word
 
 describe('number_map_to_word_over_26',function(){
   var collection_a = [1,13,27,30,25,27];
-  var collection_b = ['a','m','aa','ad','y','aa']
+  var collection_b = ['a','m','aa','ad','y','aa'];
 
   it('字母表映射2',function(){
     var result = number_map_to_word_over_26(collection_a);
