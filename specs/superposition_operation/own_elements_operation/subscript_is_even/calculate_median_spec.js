5e3f9ded94de5844d10edba94dab229ac563ae13
fgcui <fgcui1204@163.com>
2015-03-18 11:40:26 +0800

    add calculate_median test

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
@@ -1,3 +1,11 @@
-/**
- * Created by fgcui on 3/18/15.
- */
+'use strict';
+var calculate_median = require('../../../practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js');
+
+describe('calculate_median_spec',function(){
+  var collection_a = [1,2,3,4,5,6];
+
+  it('算中位数',function(){
+    var result = calculate_median(collection_a);
+    expect(result).toEqual(3.5);
+  });
+});
