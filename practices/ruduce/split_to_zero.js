5c6c5e1c753945b1569b536fb9ea130dc303a2c4
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-20 09:29:49 +0800

    complete split_to_zero test

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,3 +1,7 @@
-/**
- * Created by zhzhang on 3/19/15.
- */
+'use strict';
+
+function collect_max_number(number) {
+  //在这里写入代码
+}
+
+module.exports = collect_max_number;
--- specs/ruduce/split_to_zero_spec.js
+++ specs/ruduce/split_to_zero_spec.js
@@ -7,13 +7,15 @@ describe('split_to_zero', function () {
   var collection_a = [0.8, 0.6, 0.4, 0.2, 0];
   var collection_b = [0.7, 0.4, 0.1, -0.2];
 
-  it('选出给定数字集合元素的最大值', function() {
+  it('根据给定数字无限分割至小于等于0', function() {
+    spyOn(Math, 'random').andReturn(0.1);
 
     var result = split_to_zero(0.2);
     expect(result).toEqual(collection_a);
   });
 
-  it('选出给定数字集合元素的最大值', function() {
+  it('根据给定数字无限分割至小于等于0', function() {
+    spyOn(Math, 'random').andReturn(0.1);
 
     var result = split_to_zero(0.3);
     expect(result).toEqual(collection_b);
