df1a41b396ccb458a5f025f51464dee77b9ffe1d
zhzhang <xiaoyanzhuzzh@sina.com>
2015-03-20 09:24:05 +0800

    complete split_to_zero test

--- specs/ruduce/split_to_zero_spec.js
+++ specs/ruduce/split_to_zero_spec.js
@@ -1,3 +1,22 @@
-/**
- * Created by zhzhang on 3/19/15.
- */
+'use strict';
+
+var split_to_zero = require('../../practices/ruduce/split_to_zero.js');
+
+describe('split_to_zero', function () {
+
+  var collection_a = [0.8, 0.6, 0.4, 0.2, 0];
+  var collection_b = [0.7, 0.4, 0.1, -0.2];
+
+  it('选出给定数字集合元素的最大值', function() {
+
+    var result = split_to_zero(0.2);
+    expect(result).toEqual(collection_a);
+  });
+
+  it('选出给定数字集合元素的最大值', function() {
+
+    var result = split_to_zero(0.3);
+    expect(result).toEqual(collection_b);
+  });
+});
+
