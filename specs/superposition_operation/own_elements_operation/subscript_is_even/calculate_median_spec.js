1f72268e309d9b5eddf06ef448c649267c0137e2
fgcui <fgcui1204@163.com>
2015-05-28 18:29:51 +0800

    modify median spec

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
@@ -3,15 +3,15 @@ var calculate_median = require('../../../../practices/superposition_operation/ow
 
 describe('calculate_median_spec', function() {
   var collection_a = [1, 2, 3, 4, 5, 6];
-  var collection_b = [2, 3, 4, 5, 6, 7, 8, 9, 0];
+  var collection_b = [2, 3, 4, 5, 6, 7, 8, 9];
 
-  it('集合个数为偶数时，算中位数', function() {
+  it('集合中下标为偶数的元素个数为奇数个时，计算所有下标为偶数的元素的中位数', function() {
     var result = calculate_median(collection_a);
-    expect(result).toEqual(4);
+    expect(result).toEqual(3);
   });
 
-  it('集合个数为奇数时，算中位数', function() {
+  it('集合中下标为偶数的元素个数为偶数个时，计算所有下标为偶数的元素的中位数', function() {
     var result = calculate_median(collection_b);
-    expect(result).toEqual(6);
+    expect(result).toEqual(5);
   });
 });
