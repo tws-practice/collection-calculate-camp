1e33d10bae48ede0255dad5fad1e22d331301443
fgcui <fgcui1204@163.com>
2015-05-28 18:23:36 +0800

    remove a  spec and modify result

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_average_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_average_spec.js
@@ -3,15 +3,10 @@ var calculate_average = require('../../../../practices/superposition_operation/o
 
 describe('calculate_average_spec', function() {
   var collection_a = [1, 2, 3, 4, 5, 6];
-  var collection_b = [2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-  it('集合个数为偶数时，算平均数', function() {
+  it('计算所有下标为偶数的元素的平均数', function() {
     var result = calculate_average(collection_a);
-    expect(result).toEqual(4);
+    expect(result).toEqual(3);
   });
 
-  it('集合个数为奇数时，算平均数', function() {
-    var result = calculate_average(collection_b);
-    expect(result).toEqual(12);
-  });
 });
