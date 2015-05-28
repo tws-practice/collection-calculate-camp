67cb6b99c11f26f8f9c6ee3d979cdb914bad417d
fgcui <fgcui1204@163.com>
2015-05-28 19:14:40 +0800

    modify describe of average test

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_average_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_average_spec.js
@@ -4,9 +4,9 @@ var calculate_average = require('../../../../practices/superposition_operation/o
 describe('calculate_average_spec', function() {
   var collection_a = [1, 2, 3, 4, 5, 6];
 
-  it('计算所有下标为偶数的元素的平均数', function() {
+  it('计算第偶数个元素的平均数', function() {
     var result = calculate_average(collection_a);
-    expect(result).toEqual(3);
+    expect(result).toEqual(4);
   });
 
 });
