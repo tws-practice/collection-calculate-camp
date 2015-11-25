b45b466026ef069923abda3d7bdf9f57e966e188
jtong <tj19832@gmail.com>
2015-11-25 18:11:41 +0800

    jtong - fix issue#15

--- specs/section_2/practice_3_spec.js
+++ specs/section_2/practice_3_spec.js
@@ -2,7 +2,7 @@
 
 var count_same_elements = require("../../practices/section_2/practice_3/practice.js");
 
-xdescribe('practice-2-3', function() {
+describe('practice-2-3', function() {
 
   var collection = [
     "a", "a", "a",
