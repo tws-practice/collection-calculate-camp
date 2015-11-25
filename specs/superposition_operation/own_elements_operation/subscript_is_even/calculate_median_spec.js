cbbf7c8181bbcef2fd7f8b5ad03ad73127d01a45
jtong <tj19832@gmail.com>
2015-11-25 18:15:47 +0800

    jtong - fix issue#17

--- specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
+++ specs/superposition_operation/own_elements_operation/subscript_is_even/calculate_median_spec.js
@@ -5,12 +5,12 @@ describe('calculate_median_spec', function() {
   var collection_a = [1, 2, 3, 4, 5, 6];
   var collection_b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  it('集合中第偶数个元素的个数为奇数时，计算第偶数个元素的中位数', function() {
+  it('集合中第偶数个元素的个数为奇数时，计算所有第偶数个元素的中位数', function() {
     var result = calculate_median(collection_a);
-    expect(result).toEqual(3);
+    expect(result).toEqual(4);
   });
 
-  it('集合中第偶数个元素的个数为偶数时，计算第偶数个元素的中位数', function() {
+  it('集合中第偶数个元素的个数为偶数时，计算所有第偶数个元素的中位数', function() {
     var result = calculate_median(collection_b);
     expect(result).toEqual(5);
   });
