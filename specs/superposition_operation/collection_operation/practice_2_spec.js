2d2c1bb55dbc84d42570165b33e671ff94cb38fc
Liuhaiyan6 <2391348695@qq.com>
2016-07-29 14:46:57 +0800

    commit

--- specs/superposition_operation/collection_operation/practice_2_spec.js
+++ specs/superposition_operation/collection_operation/practice_2_spec.js
@@ -2,14 +2,14 @@
 
 var hybrid_operation_to_uneven = require("../../../practices/superposition_operation/collection_operation/practice_2.js");
 
-describe('hybrid_operation_to_uneven', function() {
+ddescribe('hybrid_operation_to_uneven', function () {
 
-  var collection = [1,5,7,12,11,35,54,67,70];
+  var collection = [1, 5, 7, 12, 11, 35, 54, 67, 70];
 
-  it('每一个奇数*3+2', function() {
+  it('每一个奇数*3+2', function () {
 
     var result = hybrid_operation_to_uneven(collection);
 
-    expect(result).toEqual([5,17,23,35,107,203]);
+    expect(result).toEqual([5, 17, 23, 35, 107, 203]);
   });
 });
--- specs/superposition_operation/interval_operation/amount_even_spec.js
+++ specs/superposition_operation/interval_operation/amount_even_spec.js
@@ -2,11 +2,11 @@
 
 var amount_even = require("../../../practices/superposition_operation/interval_operation/amount_even.js");
 
-describe('amount_even', function() {
+describe('amount_even', function () {
 
-  var collection = [1,2,3,4,5,6,7,8,9,10];
+  var collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  it('1-10的偶数算总数', function() {
+  it('1-10的偶数算总数', function () {
 
     var result = amount_even(collection);
 
