0a72fc80cf5ee2fe9aadd3ef01156c92b189e1c9
James <tj19832@gmail.com>
2017-07-29 10:58:53 +0800

    fix spec text describe"交集"->"并集"

--- specs/collectionOperator/get_union_spec.js
+++ specs/collectionOperator/get_union_spec.js
@@ -8,7 +8,7 @@ describe('get_union', function () {
   var collection_b = [5, 78, 28, 19, 23];
   var collection_c = [10, 27, 28, 19, 5, 78, 23];
 
-  it('弹出两个集合的交集', function() {
+  it('弹出两个集合的并集', function() {
 
     var result = get_union(collection_a, collection_b);
     expect(result).toEqual(collection_c);
